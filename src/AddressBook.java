7b716aa3de5c4330b0680b4877207efb1ab2c6a2
Jalal <jalalmrad2002@gmail.com>
2022-09-25 19:10:36 -0400

    new

---
+++ src/AddressBook.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class AddressBook {
+
+    ArrayList<BuddyInfo> buddylist;
+
+    public AddressBook(){
+
+        buddylist = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null){
+
+            buddylist.add(buddy);
+        }
+
+    }
+
+    public void removeBuddy(int index){
+        if(index >=0 && index<buddylist.size()){
+            buddylist.remove(index);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Jalal", 613,"Carleton");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
+
+
+    }
+}
